556c67db33bd60db1f85719163c8e2d741a7215c
Alban <albanmc573@gmail.com>
2025-06-08 04:17:31 +0200

    fix: config for Railway deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/playercount', async (req, res) => {
   try {
